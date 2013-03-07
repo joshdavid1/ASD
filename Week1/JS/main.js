4d49740c500cd7d10a309c1c1a9beb1b243c02b5
Josh <joshdavid1@fullsail.edu>
2013-03-06 23:29:28 -0800

    Update failed previously.

--- Week1/JS/main.js
+++ Week1/JS/main.js
@@ -4,3 +4,6 @@ Class: ASD with Doug Arley (March '13)
 Week: Project 1
 Topic: jQuery, HTML, and CSS framework files
 */
+$(document).on('pageinit',function() {
+        // Put jQuery in here to fire it after the page loads.
+    });
