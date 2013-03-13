29fef811a05d137872983bd905e22d0fc5e5fef2
Josh <joshdavid1@fullsail.edu>
2013-03-12 18:03:58 -0700

    Added data-role pages.
    Created week 2 folder and updated JS folder with libs.

---
+++ Week2/JS/main.js
@@ -0,0 +1,17 @@
+/*
+Author: Josh Wilcox
+Class: ASD with Doug Arley (March '13)
+Week: Project 2
+Topic: converting from VFW CRUD to jQuery and JQM.
+*/
+$('#main').on('pageinit',function() {
+        // Put jQuery in here for the home page.
+});
+
+$('#addPage').on('pageinit',function() {
+        // Put jQuery in here for the add page, which contains the form.
+});
+
+$('#options').on('pageinit',function() {
+        // Put jQuery in here for the options page.
+});
