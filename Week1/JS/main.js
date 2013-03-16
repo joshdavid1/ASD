54cc6d40e291e0cfa44725834fd2d9f1570475dc
Josh <joshdavid1@fullsail.edu>
2013-03-15 17:01:37 -0700

    All 4 button events now firing.

--- Week1/JS/main.js
+++ Week1/JS/main.js
@@ -11,7 +11,7 @@ $('#main').on('pageinit',function() {
 $('#addPage').on('pageinit',function() {
 
 	$("#showLadder").click(function() {
-	  alert("Handler for Show Ladder Button called.");
+		alert("Handler for Show Ladder Button called.");
 	});
 
 	//Save Function
@@ -31,56 +31,56 @@ $('#addPage').on('pageinit',function() {
 	// };
 
 	$("#saveRung").click(function() {
-	  alert("Handler for Save Button called.");
+		alert("Handler for Save Button called.");
 	});
 
 
 	//Clear rungs
 	$("#clearFormButton").click(function() {
-	  alert("Handler for Clear Form Button called.");
+		alert("Handler for Clear Form Button called.");
 	});
 
 	$("#clearDataButton").click(function() {
-	  alert("Handler for Clear Data Button called.");
+		alert("Handler for Clear Data Button called.");
 	});
-
+});
 $('#ladderView').on('pageinit',function() {
-    var showRungs = function(){
+    // var showRungs = function(){
 
 
 console.log("in showRungs function");
 
 
-	for(var i=0, len=rungs.length; i<len; i++){
-	console.log("in rung for loop, cycle " + i);
-		var newLi = document.createElement('li');
+// 	for(var i=0, len=rungs.length; i<len; i++){
+// 	console.log("in rung for loop, cycle " + i);
+// 		var newLi = document.createElement('li');
 	    		
-		$('list').appendChild(newLi);
+// 		$('list').appendChild(newLi);
 	  
-	    var heading = document.createElement('h3');
-	    heading.innerHTML = rungs[i].rung;
-		newLi.appendChild(heading);
-	    var pinterval = document.createElement('p');
-	    pinterval.innerHTML = rungs[i].interval;
-	    newLi.appendChild(pinterval);
-	    var pFav = document.createElement('p');
-	    pFav.innerHTML = "Is this a goal? " + rungs[i].goal;
-	    newLi.appendChild(pFav);
-	}
-};
+// 	    var heading = document.createElement('h3');
+// 	    heading.innerHTML = rungs[i].rung;
+// 		newLi.appendChild(heading);
+// 	    var pinterval = document.createElement('p');
+// 	    pinterval.innerHTML = rungs[i].interval;
+// 	    newLi.appendChild(pinterval);
+// 	    var pFav = document.createElement('p');
+// 	    pFav.innerHTML = "Is this a goal? " + rungs[i].goal;
+// 	    newLi.appendChild(pFav);
+// 	}
+// };
 
 
 //Check status of radio button
 
-var getGoal = function(){
-console.log("inside getGoal function");
-	var goalCheck = document.querySelector("#radio-choice");
-	var status;
-	if(goalCheck.checked){
-		status = "Yes";
-	}else{
-		status = "No";
-	};
-	return status;
-};
+// var getGoal = function(){
+// console.log("inside getGoal function");
+// 	var goalCheck = document.querySelector("#radio-choice");
+// 	var status;
+// 	if(goalCheck.checked){
+// 		status = "Yes";
+// 	}else{
+// 		status = "No";
+// 	};
+// 	return status;
+// };
 });
